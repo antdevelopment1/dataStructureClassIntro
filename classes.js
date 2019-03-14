57308d690bf07fe58e360d844b775a88acf60bc9
April Copes <antdevelopment1@gmail.com>
2019-03-14 18:21:53 -0400

    generic class

---
+++ classes.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(firstName, lastName, year) {
+        this.firstName = firstName;
+        this.lastName - lastName;
+        this.year = year;
+    }
+}
+
+let firstStudent = new Student('April', 'Copes', '1991');
+let secondStudent = new Student('Lauren', 'Wilkerson', '1990');
+
+console.log(firstStudent.firstName);
+console.log(secondStudent);
+
+
